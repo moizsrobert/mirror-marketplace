ab84017529775a35312618b4ee05d9bab710a0c9
Moizs Róbert <moizsrobert@gmail.com>
2021-10-07 19:38:38 +0200

    Initial commit

---
+++ src/main/java/mirrors/mirrorsbackend/MirrorsBackendApplication.java
@@ -0,0 +1,13 @@
+package mirrors.mirrorsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MirrorsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MirrorsBackendApplication.class, args);
+    }
+
+}
